5639e1654cce7075b9193b572f1d063303e78c3b
nicola-ferrarese <f.nicola98@gmail.com>
2024-08-11 16:09:01 +0200

    [CD] updated workflow

--- tyrianapp.js
+++ tyrianapp.js
@@ -1,7 +1,7 @@
 console.log("launching");
 import {
     TyrianApp
-} from './target/scala-3.4.2/pps-tyranexample-fastopt/main.js';
+} from './target/scala-3.4.2/pps-tyranexample-opt/main.js';
 
 TyrianApp.launch("myapp");
 
