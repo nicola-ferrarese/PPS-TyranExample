3ed98f31ad7102dd717db2bc9b0bdb3deb89a667
nicola-ferrarese <nicola-ferrarese@users.noreply.github.com>
2024-08-11 12:12:12 +0000

    deploy: 774a3b27e3ac92cd7b5088f61a05d97edf23c429

---
+++ tyrianapp.js
@@ -0,0 +1,8 @@
+import {
+  TyrianApp
+} from './target/scala-3.4.2/examples-counter-fastopt/main.js';
+
+console.log("Tyrian app script loaded");
+
+TyrianApp.launch("myapp");
+console.log("Tyrian app launched");
