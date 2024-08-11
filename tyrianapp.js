b26cda3d0b05558451f664b20925f6ebe91b304a
nicola-ferrarese <46693501+nicola-ferrarese@users.noreply.github.com>
2024-08-11 14:18:50 +0000

    Deploying to gh-pages from @ nicola-ferrarese/PPS-TyranExample@43da0b4eb3fec684db0680e8ac5b169b841a4b33 🚀

--- tyrianapp.js
+++
@@ -1,8 +0,0 @@
-import {
-  TyrianApp
-} from './target/scala-3.4.2/pps-tyranexample-fastopt/main.js';
-
-console.log("Tyrian app script loaded");
-
-TyrianApp.launch("myapp");
-console.log("Tyrian app launched");
